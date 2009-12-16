6babe718cb0280ca7df10cb92ffda1457b730001
Casey Dreier <cdreier@gmail.com>
2009-12-15 23:45:39 -0500

    Updating error display for form submission.  Adding tests and methods for message types.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,50 @@
+require 'pastie_client'
 class Message < ActiveRecord::Base
-  validates_presence_of :body
+  attr_accessor :message_type
   
-  default_scope :order => 'created_at DESC'
+  # Validations #
+  validates_presence_of :body
+  validate :valid_message_type
 
-  def self.calculate_rankings
-    # do some crazy math here
-    # manipulate a pdf, create a graphic file, upload to S3, etc.
-    return 42
+  # Callbacks #
+  after_validation :send_code_to_pastie
+  
+  # Scopes #
+  default_scope :order => 'created_at DESC'
+  
+  # Class methods #
+  class << self
+    def calculate_rankings
+      # do some crazy math here
+      # manipulate a pdf, create a graphic file, upload to S3, etc.
+      return 42
+    end
+    
+    def message_types
+      Message::Extensions::TYPES
+    end
+    
   end
+  
+  # Modules #
+  module Extensions
+    TYPES = { 0 => 'says', 1 => 'codes'}.freeze
+  end
+
+  def message_type=(msg_typ)
+    @message_type = msg_typ.to_i
+  end
+  
+  def send_code_to_pastie
+    return true if message_type != 1
+    
+  end
+  
+  private
+  
+  def valid_message_type
+    errors.add_to_base("Invalid message type") unless Message::Extensions::TYPES.keys.include?(message_type) or message_type.blank?
+  end
+  
   
 end
--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -10,4 +10,29 @@ class MessageTest < ActiveSupport::TestCase
     good_message = Message.new(:body => "hey there!", :user_name => "bob")
     assert good_message.valid?
   end
+  
+  test "should respond to message types" do
+    assert Message.respond_to?(:message_types)
+  end
+  
+  test "message types should return a hash of message types" do
+    assert Message.message_types.is_a?(Hash)
+  end
+  
+  test "message type should accept only certain values " do
+    message = Message.new(:body => 'test')
+    message.message_type = 738 #invalid
+    assert !message.valid?
+  end
+  
+  test "message type should allow certain values" do
+    message = Message.new(:body => 'test')
+    message.message_type = 0 #valid
+    assert message.valid?
+     
+    message = Message.new(:body => 'test')
+    message.message_type = "0" #valid
+    assert message.valid?
+  end
+  
 end
